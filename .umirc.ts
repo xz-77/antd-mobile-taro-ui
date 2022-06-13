af63cd9c664bace5e2207aa7ed53d7fada49a0df
7 <>
2022-06-13 16:54:53 +0800

    feat: 安装lerna

--- .umirc.ts
+++ .umirc.ts
@@ -8,8 +8,19 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
-  alias: {
-    'antd-mobile': path.resolve(__dirname, './packages/src/index.ts'),
+  resolve: {
+    includes: ['docs', 'packages/ui/src'],
   },
+  alias: {
+    'antd-mobile': path.resolve(__dirname, './packages/ui/src/index.ts'),
+    demos: path.resolve(__dirname, './packages/ui/src/demos'),
+    // '@/': path.resolve(__dirname, './packages/ui/src/'),
+    '@/styles': path.resolve(__dirname, './packages/ui/src/styles'),
+    '@/utils': path.resolve(__dirname, './packages/ui/src/utils'),
+  },
+  locales: [
+    ['en', 'English'],
+    ['zh', '中文'],
+  ],
   // more config: https://d.umijs.org/config
 });
