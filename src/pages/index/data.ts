da9c90bb6123eeac01ee30fa4ad08b4338893df1
7 <x>
2022-07-28 15:30:30 +0800

    feat: 修改数据结构

--- src/pages/index/data.ts
+++ src/pages/index/data.ts
@@ -1,52 +1,82 @@
 export const data = [
   {
-    title: 'Button按钮',
-    desc: '不支持type、onMouseDown、onMouseUp属性',
-    url: '/pages/button/index',
+    name: '通用',
+    items: [
+      {
+        title: 'Button按钮',
+        desc: '不支持type、onMouseDown、onMouseUp属性',
+        url: '/pages/button/index',
+      },
+    ],
   },
   {
-    title: 'Checkbox复选框',
-    desc: '',
-    url: '/pages/checkbox/index',
+    name: '布局',
+    items: [
+      {
+        title: 'AutoCenter 自动居中',
+        desc: '',
+        url: '/pages/auto-center/index',
+      },
+      {
+        title: 'Divider 分割线',
+        desc: '',
+        url: '/pages/divider/index',
+      },
+      {
+        title: 'Space间距',
+        desc: '',
+        url: '/pages/space/index',
+      },
+    ],
   },
   {
-    title: 'Radio单选框',
-    desc: '',
-    url: '/pages/radio/index',
+    name: '导航',
+    items: [],
   },
   {
-    title: 'Space间距',
-    desc: '',
-    url: '/pages/space/index',
+    name: '信息展示',
+    items: [
+      {
+        title: 'List列表',
+        desc: 'antd-mobile的demo4和demo5不支持展示',
+        url: '/pages/list/index',
+      },
+    ],
   },
   {
-    title: 'DotLoading点状加载中',
-    desc: '由于小程序不支持svg，这里使用css3动画，不支持自动适配字号',
-    url: '/pages/dot-loading/index',
+    name: '信息录入',
+    items: [
+      {
+        title: 'Checkbox复选框',
+        desc: '',
+        url: '/pages/checkbox/index',
+      },
+      {
+        title: 'Radio单选框',
+        desc: '',
+        url: '/pages/radio/index',
+      },
+    ],
   },
   {
-    title: 'List列表',
-    desc: 'antd-mobile的demo4和demo5不支持展示',
-    url: '/pages/list/index',
-  },
-  {
-    title: 'Mask遮罩层',
-    desc: '有修改精简实现方式，demo展示是一样的',
-    url: '/pages/mask/index',
-  },
-  {
-    title: 'Popup弹出层',
-    desc: 'react-spring动画不能复用，使用transition实现动画',
-    url: '/pages/popup/index',
-  },
-  {
-    title: 'Divider 分割线',
-    desc: '',
-    url: '/pages/divider/index',
-  },
-  {
-    title: 'AutoCenter 自动居中',
-    desc: '',
-    url: '/pages/auto-center/index',
+    name: '反馈',
+    items: [
+      {
+        title: 'DotLoading点状加载中',
+        desc: '由于小程序不支持svg，这里使用css3动画，不支持自动适配字号',
+        url: '/pages/dot-loading/index',
+      },
+
+      {
+        title: 'Mask遮罩层',
+        desc: '有修改精简实现方式，demo展示是一样的',
+        url: '/pages/mask/index',
+      },
+      {
+        title: 'Popup弹出层',
+        desc: 'react-spring动画不能复用，使用transition实现动画',
+        url: '/pages/popup/index',
+      },
+    ],
   },
 ];
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,18 +6,22 @@ import { data } from './data';
 
 const Index = () => {
   return (
-    <List>
-      {data.map(item => (
-        <List.Item
-          prefix={<View style={{ width: 30, height: 30, backgroundColor: '#1677ff', borderRadius: '100%' }} />}
-          key={item.title}
-          description={item.desc}
-          onClick={() => Taro.navigateTo({ url: item.url })}
-        >
-          {item.title}
-        </List.Item>
+    <>
+      {data.map(content => (
+        <List header={content.name} mode='card' key={content.name}>
+          {content.items.map(item => (
+            <List.Item
+              prefix={<View style={{ width: 30, height: 30, backgroundColor: '#1677ff', borderRadius: '100%' }} />}
+              key={item.title}
+              description={item.desc}
+              onClick={() => Taro.navigateTo({ url: item.url })}
+            >
+              {item.title}
+            </List.Item>
+          ))}
+        </List>
       ))}
-    </List>
+    </>
   );
 };
 
