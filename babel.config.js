2ff929b0e795cf5b4fc4789cee84ede719e39615
7 <x>
2022-08-04 16:32:09 +0800

    chore: 优化构建命令

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,18 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
-module.exports = {
-  presets: [
-    [
-      'taro',
-      {
-        framework: 'react',
-        ts: true,
-      },
-    ],
-  ],
-};
+
+const env = process.env.NODE_ENV;
+module.exports =
+  env === 'components'
+    ? {}
+    : {
+        presets: [
+          [
+            'taro',
+            {
+              framework: 'react',
+              ts: true,
+            },
+          ],
+        ],
+      };
