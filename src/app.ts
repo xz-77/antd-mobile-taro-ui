63b249fbefb3c3fd03d9f9d2b02e2d1f1a5a5319
7 <x>
2022-09-24 21:57:18 +0800

    fix: 兼容h5无法引入iconfont样式文件问题

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import './styles/miniprogram/theme-default.less';
 import './styles/h5/theme-default.less';
+// TODO: 兼容h5不引入iconfont样式文件问题
+import 'antd-mobile-taro-icons/lib/es/styles/iconfont.css';
 import './app.less';
 
 class App extends Component {
