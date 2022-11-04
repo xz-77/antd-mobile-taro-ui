92827a829b1249d06e0608a6dff6ee6d2986f75e
7 <x>
2022-11-04 10:52:22 +0800

    feat: 实现collapse动画的解决方案

--- src/components/collapse/collapse.tsx
+++ src/components/collapse/collapse.tsx
@@ -1,16 +1,14 @@
-import React, { FC, ReactElement, ComponentProps } from 'react';
+import React, { FC, ReactElement, ComponentProps, useState, useRef } from 'react';
 import { NativeProps, withNativeProps } from 'antd-mobile/es/utils/native-props';
 import { UnfoldIcon } from 'antd-mobile-taro-icons';
 import classNames from 'classnames';
-// import { useSpring, animated } from '@react-spring/web';
 import { usePropsValue } from 'antd-mobile/es/utils/use-props-value';
-// import { useShouldRender } from 'antd-mobile/es/utils/should-render';
-// import { useIsomorphicUpdateLayoutEffect } from 'antd-mobile/es/utils/use-isomorphic-update-layout-effect';
+import { useShouldRender } from 'antd-mobile/es/utils/should-render';
+import { useIsomorphicUpdateLayoutEffect } from 'antd-mobile/es/utils/use-isomorphic-update-layout-effect';
 import { traverseReactNode } from 'antd-mobile/es/utils/traverse-react-node';
 import { ITouchEvent, View } from '@tarojs/components';
-// import Taro, { useResize } from '@tarojs/taro';
-// import { useFavicon, useMount } from 'ahooks';
-import { useShouldRender } from 'antd-mobile/es/utils/should-render';
+import Taro from '@tarojs/taro';
+import { useMount } from 'ahooks';
 import List from '../list';
 
 const classPrefix = `adm-collapse`;
@@ -37,58 +35,70 @@ const CollapsePanelContent: FC<{
   children?: React.ReactNode;
 }> = props => {
   const { visible } = props;
-  // const innerRef = useRef<HTMLDivElement>(null);
+  const innerRef = useRef<HTMLDivElement>(null);
   const shouldRender = useShouldRender(visible, props.forceRender, props.destroyOnClose);
-  // const [height, setHeight] = useState(0);
+  const [height, setHeight] = useState<number | 'auto'>(0);
 
-  // const nodeRef = useRef<Taro.NodesRef | null>(null);
+  const nodeRef = useRef<Taro.NodesRef | null>(null);
 
-  // useMount(() => {
-  //   setTimeout(() => {
-  //     const inner = innerRef.current;
+  useMount(() => {
+    setTimeout(() => {
+      const inner = innerRef.current;
 
-  //     if (!inner) return;
-  //     // TODO:找一个更合适的方式 做节点的唯一标识
-  //     // @ts-ignore
-  //     if (inner?.sid) {
-  //       nodeRef.current = Taro.createSelectorQuery()
-  //         // @ts-ignore
-  //         .select(`#${inner?.sid}`);
-  //     } else {
-  //       console.error(`HTMLDivElement: Taro node can no find sid`);
-  //     }
+      if (!inner) return;
+      // TODO:找一个更合适的方式 做节点的唯一标识
+      // @ts-ignore
+      if (inner?.sid) {
+        nodeRef.current = Taro.createSelectorQuery()
+          // @ts-ignore
+          .select(`#${inner?.sid}`);
+      } else {
+        console.error(`HTMLDivElement: Taro node can no find sid`);
+      }
 
-  //     if (!visible) return;
-  //     if (!nodeRef.current) return;
-  //     nodeRef.current.boundingClientRect(rect => setHeight(rect.height)).exec();
-  //   }, 0);
-  // });
+      if (!visible) return;
+      if (!nodeRef.current) return;
+      nodeRef.current.boundingClientRect(rect => setHeight(rect.height)).exec();
+    }, 0);
+  });
 
-  // useIsomorphicUpdateLayoutEffect(() => {
-  //   if (!nodeRef.current) return;
-  //   if (visible) {
-  //     Taro.nextTick(() => {
-  //       if (!nodeRef.current) return;
-  //       nodeRef.current
-  //         .boundingClientRect(rect => {
-  //           console.log(rect.height);
-  //           setHeight(rect.height);
-  //         })
-  //         .exec();
-  //     });
-  //   } else {
-  //     setHeight(0);
-  //   }
-  // }, [visible]);
+  useIsomorphicUpdateLayoutEffect(() => {
+    if (!nodeRef.current) return;
+    if (visible) {
+      if (!nodeRef.current) return;
+      Taro.nextTick(() => {
+        if (!nodeRef.current) return;
+        nodeRef.current
+          .boundingClientRect(rect => {
+            setHeight(rect.height);
+          })
+          .exec();
+      });
+
+      setTimeout(() => {
+        setHeight('auto');
+      }, 200);
+    } else {
+      nodeRef.current
+        .boundingClientRect(rect => {
+          setHeight(rect.height);
+        })
+        .exec();
+
+      setTimeout(() => {
+        setHeight(0);
+      }, 200);
+    }
+  }, [visible]);
 
   return (
     <View
       className={`${classPrefix}-panel-content`}
       style={{
-        height: visible ? 'auto' : 0,
+        height,
       }}
     >
-      <View className={`${classPrefix}-panel-content-inner`}>
+      <View className={`${classPrefix}-panel-content-inner`} ref={innerRef}>
         <List.Item>{shouldRender && props.children}</List.Item>
       </View>
     </View>
